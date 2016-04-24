b1af9eb7a2b55c102f61040393ccda53b095fd67
Misa Ogura <misa.ogura01@gmail.com>
2016-04-24 15:12:02 +0100

    Delete cohortInfo.js

--- test/e2e/userFeatures.js
+++ test/e2e/userFeatures.js
@@ -4,18 +4,18 @@ describe('user tracker', function() {
     expect(browser.getTitle()).toEqual('Github Grabber');
   });
 
-  // it('has a title', function() {
-  //   browser.get('/');
-  //   var users = $$('#attribute-container');
-  //   expect(users.first().getText()).toEqual('SimonGlancy\nRepos: 32\nFollowers: 5');
-  //   expect(users.last().getText()).toEqual('MisaOgura\nRepos: 44\nFollowers: 6');
-  // });
-  //
-  // it('adds searched user', function() {
-  //    browser.get('/');
-  //    $('#inputText').sendKeys('SimonGlancy\nRepos: 32\nFollowers: 5');
-  //    $('#Go').click();
-  //    var users = $$('#attribute-container');
-  //    expect(users.first().getText()).toMatch('SimonGlancy\nRepos: 32\nFollowers: 5');
-  //  });
+  it('has a title', function() {
+    browser.get('/');
+    var users = $$('#attribute-container');
+    expect(users.first().getText()).toEqual('SimonGlancy\nRepos: 32\nFollowers: 5');
+    expect(users.last().getText()).toEqual('MisaOgura\nRepos: 44\nFollowers: 6');
+  });
+
+  it('adds searched user', function() {
+     browser.get('/');
+     $('#inputText').sendKeys('SimonGlancy\nRepos: 32\nFollowers: 5');
+     $('#Go').click();
+     var users = $$('#attribute-container');
+     expect(users.first().getText()).toMatch('SimonGlancy\nRepos: 32\nFollowers: 5');
+   });
 });
