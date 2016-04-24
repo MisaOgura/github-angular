e9d7211f0a96f74a46df622742d1ff2e5666a9f7
Misa Ogura <misa.ogura01@gmail.com>
2016-04-24 19:27:54 +0100

    Cohort button available only once per session. If some members are already liseted they will not be duplicated. Implement search message to interact with end users. When there is not match it will not show blank profile, instead it will display a message

--- public/js/controllers/UsersController.js
+++ public/js/controllers/UsersController.js
@@ -3,25 +3,39 @@ userApp.controller('UsersController', ['SearchService', function(SearchService) 
   var self = this;
 
   self.users = [];
+  var febCohortUsed = false;
+  var message = document.getElementById('searchMessage');
 
   self.cohort = function() {
-    SearchService.getCohort()
-    .then(function(userData){
-      userData.forEach(function(member) {
-        self.users.push(member);
+    if (!febCohortUsed){
+      SearchService.getCohort()
+      .then(function(userData){
+        userData.forEach(function(member) {
+          if (!isAlreadyListed(member.login)) {
+            message.textContent = 'Grabbed the whole Feb\'16 cohort!';
+            self.users.push(member);
+          }
+        });
       });
-    });
+      febCohortUsed = true;
+    }
   };
 
   self.search = function(username) {
+    document.getElementById('searchBox').value = '';
+    document.getElementById('searchMessage').textContent = '';
     SearchService.getUser(username)
     .then(function(userData){
-      if (isAlreadyListed(userData.login)) {
-        var user = document.getElementById(userData.login);
+      var user = document.getElementById(userData.login);
+      if (userData.login === undefined) {
+        message.textContent = 'Could not find a match';
+      }
+      else if (isAlreadyListed(userData.login)) {
         user.scrollIntoView(true);
         user.style.border = 'black solid 2px';
       }
       else {
+        message.textContent = 'Grabbed ' + userData.login + '!';
         self.users.unshift(userData);
       }
     });
